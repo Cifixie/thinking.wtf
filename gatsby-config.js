0707546c5d368925a0b1c295e68240d25613f047
Tommi <mato12@hotmail.fi>
2018-09-03 00:05:51 +0300

    Generate Pages by BlogPost Slugs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,21 @@
 module.exports = {
+  siteMetadata: {
+    title: "Thinking.wtf",
+    siteUrl: "https://thinking.wtf/",
+    description: `My Diary`
+  },
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-netlify",
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: "71kh0aiiimyk",
+        accessToken:
+          "f015819de117ef8991a212b42ac67bb614f30547b2d568fe911843761091e42b"
+      }
+    },
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
---
+++ gatsby-node.js
@@ -0,0 +1,32 @@
+const path = require(`path`);
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators;
+
+  const loadBlogPosts = new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allContentfulBlogPost {
+          edges {
+            node {
+              slug
+            }
+          }
+        }
+      }
+    `).then(result => {
+      result.data.allContentfulBlogPost.edges.map(({ node: { slug } }) => {
+        createPage({
+          path: `/blog/${slug}/`,
+          component: path.resolve(`./src/templates/blogPost.js`),
+          context: {
+            slug
+          }
+        });
+      });
+      resolve();
+    });
+  });
+
+  return Promise.all([loadBlogPosts]);
+};
